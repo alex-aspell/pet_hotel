2a647c3ef1244cefab20c26bb9ecd404d45fb7da
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-02-20 13:42:21 -0600

    pets post complete

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,7 @@ function onReady() {
     console.log('JQ Sourced');
     getOwner();
     $('#ownerBtn').on('click', ownerPost);
+    $('#petBtn').on('click', petPost);
 } // end onReady
 
 function ownerPost(){
@@ -38,7 +39,24 @@ function getOwner(){
 function showOwnerDropdown(owners){
     $('#ownerName').empty();
     for (let owner of owners){
-        let ownerShow = `<option value="" data-id="${owner.owner_id}">${owner.first_name} ${owner.last_name}</option>`
+        let ownerShow = `<option value="${owner.owner_id}" data-id="${owner.owner_id}">${owner.first_name} ${owner.last_name}</option>`
         $('#ownerName').append(ownerShow);
     }
+}
+
+function petPost(){
+    $.ajax({
+        type: 'POST',
+        url: '/hotel/pets',
+        data: { name: $('#petName').val(),
+                breed: $('#petBreed').val(),
+                color: $('#petColor').val(),
+                owner_id: $('#ownerName').val()
+        } 
+    }).done((response) => {
+        console.log('pets added');
+        getOwner(); 
+    }).fail((response) => {
+        console.log('pets added');
+    })
 }
