4a9b4dedabfc27a6d66b713b8a307cdf1a29cc41
Alex Aspell <alexgaspell@gmail.com>
2018-02-20 11:20:44 -0600

    owner get and post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,32 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ Sourced');
+    getOwner();
+    $('#ownerBttn').on('click', ownerPost);
 } // end onReady
+
+function ownerPost(){
+    $.ajax({
+        type: 'POST',
+        url: '/hotel/owner',
+        data: { firstname: $('#ownerFirstName').val(),
+                lastname: $('#ownerLastName').val()
+        } 
+    }).done((response) => {
+        console.log('owner added');
+        getOwner(); 
+    }).fail((response) => {
+        console.log('owner not added');
+    })
+}
+
+function getOwner(){
+    $.ajax({
+        type: 'GET',
+        url: '/hotel/owner'
+    }).done((response) => {
+        console.log('got owner', response);
+    }).fail((response) => {
+        console.log('owner get failed');
+    })
+}
