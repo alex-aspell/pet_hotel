4d1ee021899b1bc23e762801311c854474bd605d
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-02-20 11:36:26 -0600

    route par

--- server/routes/pet-router.js
+++ server/routes/pet-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const pool = require('../modules/pool');
 const bodyParser = require('body-parser');
 
